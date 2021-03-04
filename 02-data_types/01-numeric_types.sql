17802c8655e960e0f86192b836f32e9a8189de9d
sirmcclay <mcclay.design@gmail.com>
2021-03-04 10:14:09 -0300

    Force overflow on SMALLINT data type

--- 02-data_types/01-numeric_types.sql
+++ 02-data_types/01-numeric_types.sql
@@ -4,3 +4,6 @@
 -- This statement forces Postgres to assume that 2.0 must be treated as a integer
   -- the result is 2 as integer
 SELECT (2.0::INTEGER);
+
+-- Force overflow on SMALLINT data type
+SELECT (999999999999999::SMALLINT);
