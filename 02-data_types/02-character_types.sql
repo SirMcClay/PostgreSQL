b32963fb200309b0ddb6f62f6eb59e31f1dfc1b2
sirmcclay <mcclay.design@gmail.com>
2021-03-04 10:36:30 -0300

    rules of characters data type

---
+++ 02-data_types/02-character_types.sql
@@ -0,0 +1,7 @@
+-- RULES FOR CHARACTER TYPES
+  -- CHAR(5) - Store some characteres, length will always be 5 even if PG has to
+    -- insert spaces
+  -- VARCHAR - Store any length of string
+  -- VARCHAR(40) - Store a string up to 40 characters automatically remove
+    -- extra characters
+  -- TEXT - Store any length of string
