6c0bd81158801606cb98282eae327eb0061a9519
sirmcclay <mcclay.design@gmail.com>
2021-03-04 10:11:43 -0300

    data types and first statement which forces Postgres to assume that 2.0 must be treated as a integer the result is 2 as integer

---
+++ 02-data_types/01-numeric_types.sql
@@ -0,0 +1,6 @@
+-- Little trick to play with numeric types on Postgres pgAdmin
+  -- Just launch Query Tool on postgres database and write querys like below
+
+-- This statement forces Postgres to assume that 2.0 must be treated as a integer
+  -- the result is 2 as integer
+SELECT (2.0::INTEGER);
