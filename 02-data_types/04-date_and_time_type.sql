72f078a065ade39f8087643c8c1be5e7562d4a3d
sirmcclay <mcclay.design@gmail.com>
2021-03-04 12:40:48 -0300

    time without time zone rules and examples

--- 02-data_types/04-date_and_time_type.sql
+++ 02-data_types/04-date_and_time_type.sql
@@ -11,3 +11,13 @@
   SELECT ('NOV 20, 1980'::DATE); => 1980-11-20
   SELECT ('1980 NOV 20'::DATE); => 1980-11-20
   SELECT ('1980 november 20'::DATE); => 1980-11-20
+
+-- RULES FOR TIME WITHOUT TIME ZONE
+  -- '01:23 AM' -> 01:23, no time zone
+  -- '05:23 PM' -> 17:23, no time zone
+  -- '20:34' -> 20:34, no time zone
+
+-- Some possible interpretations for TIME type
+  SELECT ('01:23'::TIME); -> 01:23:00
+  SELECT ('01:23 PM'::TIME WITHOUT TIME ZONE); -> 13:23:00
+
