6835c5d5161ff392768e99276db0795f49b82cb8
sirmcclay <mcclay.design@gmail.com>
2021-03-04 11:26:52 -0300

    VARCHAR(5) - Store 'a' and 'lsdkj' and not extra spaces just the charecters

--- 02-data_types/02-character_types.sql
+++ 02-data_types/02-character_types.sql
@@ -6,6 +6,10 @@
     -- extra characters
   -- TEXT - Store any length of string
 
--- CHAR(N) - Store 'l  ' and 'llsjlfjsklfjs'
+-- CHAR(N) - Store 'l  ' and 'lls' and in first case 'l' with extra spaces up to 3
   SELECT ('l'::CHAR(3));
   SELECT ('llsjlfjsklfjs'::CHAR(3));
+
+-- VARCHAR(5) - Store 'a' and 'lsdkj' and not extra spaces just the charecters
+SELECT ('a'::VARCHAR(5));
+SELECT ('lsdkjflskdjfklsdjflksdjfa'::VARCHAR(5));
