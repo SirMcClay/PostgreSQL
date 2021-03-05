4d19176eb4ff7f6ee0d5910ccf46ca25e437e92a
sirmcclay <mcclay.design@gmail.com>
2021-03-05 11:29:27 -0300

    try to set constraint but we have a row with null value to deal with

--- 03-database_validation_constraints/01-validation.sql
+++ 03-database_validation_constraints/01-validation.sql
@@ -16,3 +16,9 @@ VALUES
 INSERT INTO products (name, department, weight)
 VALUES
 	('Pants', 'Clothes', 3);
+
+-- Try to set a constraint to price column but GOTCHA!
+  -- We cant do this if we have a row with a NULL value inside products table
+ALTER TABLE products
+ALTER COLUMN price
+SET NOT NULL;
