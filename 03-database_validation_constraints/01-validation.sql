64393e4a4aa666d52fdcee16a651e5e070a4a19c
sirmcclay <mcclay.design@gmail.com>
2021-03-05 10:27:22 -0300

    insert a product

--- 03-database_validation_constraints/01-validation.sql
+++ 03-database_validation_constraints/01-validation.sql
@@ -2,8 +2,12 @@
 CREATE TABLE products (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(40),
-	departament VARCHAR(40),
+	department VARCHAR(40),
 	price INTEGER,
 	weight INTEGER
 );
 
+-- Insert a product on our table
+INSERT INTO products (name, department, price, weight)
+VALUES
+	('Shirt', 'Clothes', 20, 1);
