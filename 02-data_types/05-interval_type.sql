0a3cc835dcf4e7eebbf524f6fbbba0913a7ea449
sirmcclay <mcclay.design@gmail.com>
2021-03-05 09:56:05 -0300

    interval calculations example with timestamp and a good tip

--- 02-data_types/05-interval_type.sql
+++ 02-data_types/05-interval_type.sql
@@ -3,7 +3,22 @@
   -- 1 D -> 1 day
   -- 1 D 1 M 1 S -> 1 day 1 minute 1 second
 
+-- TIP!! Store INTERVAL on a database is not much useful but do calculations
+  -- on DATE, TIME, TIMESTAMP are really great and very useful!
+
 -- Some examples
 SELECT ('1 day'::INTERVAL); => 1 day
 SELECT ('1 D 20 H'::INTERVAL); => 1 day 20:00:00
 SELECT ('1 D 20 H 30 M 45 S'::INTERVAL); => 1 day 20:30:45
+
+-- Useful aplications on calculations
+SELECT ('1 D 20 H 30 M 45 S'::INTERVAL) - ('1 D'::INTERVAL); => 20:30:45
+SELECT 
+	('NOV-20-1980 1:23 AM EST'::TIMESTAMP WITH TIME ZONE)
+	- 
+	('1 D'::INTERVAL); => 1980-11-19 06:23:00+00
+SELECT 
+	('NOV-20-1980 1:23 AM EST'::TIMESTAMP WITH TIME ZONE)
+	- 
+	('4 D'::INTERVAL); => 1980-11-16 06:23:00+00
+
