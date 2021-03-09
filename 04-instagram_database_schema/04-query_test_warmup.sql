b60bc8ffa17cb9eafaec3b89fd5f71433517ef29
sirmcclay <mcclay.design@gmail.com>
2021-03-09 12:58:08 -0300

    querys instagram warmup: select username and caption from user id 200 from uses and posts tables

--- 04-instagram_database_schema/04-query_test_warmup.sql
+++ 04-instagram_database_schema/04-query_test_warmup.sql
@@ -3,3 +3,10 @@ SELECT *
 FROM users
 ORDER BY id DESC
 LIMIT 3;
+
+-- Select from user id 200 a resultset with username and caption from users and
+  -- posts tables
+SELECT username, caption
+FROM users
+JOIN posts ON users.id = posts.user_id
+WHERE users.id = 200;
