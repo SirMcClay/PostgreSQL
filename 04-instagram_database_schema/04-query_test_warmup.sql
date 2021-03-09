3eb57c113476cafb61d74b358d164f1cd75d1581
sirmcclay <mcclay.design@gmail.com>
2021-03-09 13:02:17 -0300

    querys instagram warmup: show the number of likes that each user have in total

--- 04-instagram_database_schema/04-query_test_warmup.sql
+++ 04-instagram_database_schema/04-query_test_warmup.sql
@@ -10,3 +10,9 @@ SELECT username, caption
 FROM users
 JOIN posts ON users.id = posts.user_id
 WHERE users.id = 200;
+
+-- Show the number of likes that all users have in total
+SELECT username, COUNT(*)
+FROM users
+JOIN likes ON likes.user_id = users.id
+GROUP BY username;
