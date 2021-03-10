188ecc60c2dd35a6541dc481fbc8f00a7da51888
sirmcclay <mcclay.design@gmail.com>
2021-03-10 09:48:57 -0300

    internals: show all files, objects, etc...of an current database

--- 05-postgresql_internals/01-basic.sql
+++ 05-postgresql_internals/01-basic.sql
@@ -4,3 +4,7 @@ SHOW data_directory;
 -- Show all existing databases and your ids (same name of folder of that database)
 SELECT oid, datname
 FROM pg_database;
+
+-- Show all files and objects, etc which are related to a database that you query
+SELECT *
+FROM pg_class;
