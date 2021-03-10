4d464d32676fc6cadcff79c3c619a079b2d95a23
sirmcclay <mcclay.design@gmail.com>
2021-03-10 09:41:34 -0300

    internals: show all existing databases and your id

--- 05-postgresql_internals/01-basic.sql
+++ 05-postgresql_internals/01-basic.sql
@@ -1,3 +1,6 @@
 -- Show PostgreSQL directory and folder where all database files are stored
 SHOW data_directory;
 
+-- Show all existing databases and your ids (same name of folder of that database)
+SELECT oid, datname
+FROM pg_database;
