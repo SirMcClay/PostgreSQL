f378341157a30a77066e7e8ab25149dfe479eb4b
sirmcclay <mcclay.design@gmail.com>
2021-03-11 10:56:21 -0300

    CREATE EXTENSION - to take a look at data inside indexes or heap file

---
+++ 05-postgresql_internals/02-behind_the_scenes_indexes.sql
@@ -0,0 +1,5 @@
+-- EXTENSION is a piece of code that gives us some additional functionality
+  -- inside Postgresql
+  -- PAGEINSPECT gives us functionality that we use to take a look at the data
+    -- that is stored on different pages inside of either an index or a heap file
+CREATE EXTENSION pageinspect;
