76e46ed43f2de9547a91e44394cc227a20677c50
sirmcclay <mcclay.design@gmail.com>
2021-03-11 10:59:07 -0300

    shown the b-tree metapage of an index

--- 05-postgresql_internals/02-behind_the_scenes_indexes.sql
+++ 05-postgresql_internals/02-behind_the_scenes_indexes.sql
@@ -3,3 +3,7 @@
   -- PAGEINSPECT gives us functionality that we use to take a look at the data
     -- that is stored on different pages inside of either an index or a heap file
 CREATE EXTENSION pageinspect;
+
+-- Shown us the b-tree metapage of an index
+SELECT *
+FROM bt_metap('users_username_idx');
