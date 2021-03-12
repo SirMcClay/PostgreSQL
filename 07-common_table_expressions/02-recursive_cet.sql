2ef75926bb5100c92cc39b8820ed058f9fe3cec8
sirmcclay <mcclay.design@gmail.com>
2021-03-12 12:39:33 -0300

    a simple example

--- 07-common_table_expressions/02-recursive_cet.sql
+++ 07-common_table_expressions/02-recursive_cet.sql
@@ -3,4 +3,13 @@
   -- Very useful anytime you have a tree or graph-type data structure
   -- Must user UNION keyword (simple CTE dont have to use everytime)
   -- THIS IS SUPER ADVANCED - IF YOU JUST KNOW THAT THIS EXIST IS GOOD ENOUGH
-  
+
+-- A simple example:
+WITH RECURSIVE countdown(val) AS (
+	SELECT 10 AS val
+	UNION
+	SELECT val - 1 FROM countdown WHERE val > 1
+)
+SELECT *
+FROM countdown;
+-- Well this just count from the value attributed to val down to 1 just this
