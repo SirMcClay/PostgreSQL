683b7f81a147f98ab6e9dad566c74463ef951721
sirmcclay <mcclay.design@gmail.com>
2021-03-12 12:03:25 -0300

    common table expression: explainded

--- 07-common_table_expressions/01-basic.sql
+++ 07-common_table_expressions/01-basic.sql
@@ -15,3 +15,20 @@ WHERE tags.created_at < '2010-01-07';
 
 -- Here we have a problem that is a big UNION subquery inside our JOIN that
   -- turn dificult to us to understand whats going on fast enough
+
+-- So COMMON TABLE EXPRESSION is a fashin way to name a subquery and use that
+  -- as another table for simplify the understand for us and another engineer
+  -- Take a look:
+WITH tags AS (
+	SELECT user_id, created_at
+	FROM caption_tags
+	UNION ALL
+	SELECT user_id, created_at
+	FROM photo_tags
+)
+SELECT username, tags.created_at
+FROM users
+JOIN tags ON tags.user_id = users.id
+WHERE tags.created_at < '2010-01-07';
+
+-- HUH!! Way more clear!
