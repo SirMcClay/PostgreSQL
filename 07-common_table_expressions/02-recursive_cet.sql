e21605232b53cfcad951ca748e5d0329bed6d0b2
sirmcclay <mcclay.design@gmail.com>
2021-03-12 12:20:39 -0300

    notes about recursive CTE

---
+++ 07-common_table_expressions/02-recursive_cet.sql
@@ -0,0 +1,6 @@
+-- COUPLE NOTES
+  -- Recursive CTE is EXTREMALLY DIFFERENT from the simple one
+  -- Very useful anytime you have a tree or graph-type data structure
+  -- Must user UNION keyword (simple CTE dont have to use everytime)
+  -- THIS IS SUPER ADVANCED - IF YOU JUST KNOW THAT THIS EXIST IS GOOD ENOUGH
+  
