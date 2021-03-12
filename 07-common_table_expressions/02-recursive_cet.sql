080f6b70b18e16fd83941f9e5ecb3c9f9afb3fad
sirmcclay <mcclay.design@gmail.com>
2021-03-12 12:55:56 -0300

    explanation about recursive CTE

--- 07-common_table_expressions/02-recursive_cet.sql
+++ 07-common_table_expressions/02-recursive_cet.sql
@@ -13,3 +13,17 @@ WITH RECURSIVE countdown(val) AS (
 SELECT *
 FROM countdown;
 -- Well this just count from the value attributed to val down to 1 just this
+
+-- EXPLAINING
+  -- The "SELECT 10 AS val" is a initial, non recursive query
+    -- above the UNION
+  -- The "SELECT val - 1 FROM countdown WHERE val > 1" is the recursive query
+    -- below the UNION
+  -- On backstage Postgresql creates two tables RESULT and WORKING tables and 
+    -- set the columns of them with the values set on first query line inside
+    -- the parentesis like see val in "WITH RECURSIVE countdown(val)"
+  -- On our example "couuntdown" will act like a auxiliar table and "val" like
+    -- iterative value on a for loop
+  -- The recursive query will execute with "val" until the condition is not
+    -- true and then will execute the next query showing the result
+  -- This recursive CTE is more like a for loop in the world of SQL
