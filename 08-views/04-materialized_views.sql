382d0ffe36d66db6bcb1451966369e655338d0fd
sirmcclay <mcclay.design@gmail.com>
2021-03-15 11:44:59 -0300

    create materialized view and fetch the results

--- 08-views/04-materialized_views.sql
+++ 08-views/04-materialized_views.sql
@@ -15,3 +15,18 @@ LEFT JOIN posts ON posts.id = likes.post_id
 LEFT JOIN comments ON comments.id = likes.comment_id
 GROUP BY week
 ORDER BY week;
+
+-- CREATE A MATERIALIZED VIEW
+CREATE MATERIALIZED VIEW weekly_likes AS (
+  SELECT
+    date_trunc('week', COALESCE(posts.created_at, comments.created_at)) AS week,
+    COUNT(posts.id) AS num_likes_for_posts,
+    COUNT(comments.id) AS num_likes_for_comments
+  FROM likes
+  LEFT JOIN posts ON posts.id = likes.post_id
+  LEFT JOIN comments ON comments.id = likes.comment_id
+  GROUP BY week
+  ORDER BY week
+) WITH DATA;
+-- Now see the data with more fast execution and result
+SELECT * FROM weekly_likes;
