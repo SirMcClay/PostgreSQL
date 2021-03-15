948f01fb9088c2ab90138728520a5ce0b56e55bc
sirmcclay <mcclay.design@gmail.com>
2021-03-15 12:24:34 -0300

    table of accounts to test some features of transactions

--- 09-transactions/01-basic.sql
+++ 09-transactions/01-basic.sql
@@ -1,3 +1,9 @@
 -- TRANSACTIONS - Is the feature that permits we execute a number of sequential
   -- querys all of them or none of them (rollback) if something was wrong
 
+-- Lets create a table of accounts to test transaction functionality
+CREATE TABLE accounts (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
+	balance INTEGER NOT NULL
+);
