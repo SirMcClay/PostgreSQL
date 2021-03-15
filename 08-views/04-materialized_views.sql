6b13596de609881a0c31f2914ce1a52f71e1292e
sirmcclay <mcclay.design@gmail.com>
2021-03-15 11:33:18 -0300

    the query

--- 08-views/04-materialized_views.sql
+++ 08-views/04-materialized_views.sql
@@ -1,3 +1,17 @@
 -- MATERIALIZED VIEWS are usefull for run from time to time a specific VERY
   -- SLOW QUERY which store the result for be used without the need to be run
   -- just accessing the last result materialized by the view
+
+-- An EXAMPLE of query that takes some amount of time to run
+  -- This query takes 3 tables likes, posts and comments and count the number
+  -- of likes on posts and likes on comments per week using a Postgresql function
+  -- date_trunc to make possible aggregate date column per week
+SELECT
+	date_trunc('week', COALESCE(posts.created_at, comments.created_at)) AS week,
+	COUNT(posts.id) AS num_likes_for_posts,
+	COUNT(comments.id) AS num_likes_for_comments
+FROM likes
+LEFT JOIN posts ON posts.id = likes.post_id
+LEFT JOIN comments ON comments.id = likes.comment_id
+GROUP BY week
+ORDER BY week;
