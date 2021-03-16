97da238c900140664dda2a1db42a1a262dc5ef8d
sirmcclay <mcclay.design@gmail.com>
2021-03-16 11:56:10 -0300

    tools for migration

---
+++ 10-schema_migrations.sql/02-tools.sql
@@ -0,0 +1,10 @@
+-- MIGRATION LIBRARIES
+  -- There are a lot of libraries for a lot of languages to deal with migrations
+  -- we will use the node-pg-migrate distributed inside npmjs.com
+
+-- ADVISE!! - All migration tools have the ability to write the SQL automatically
+  -- for you, but highly AVOID THIS! Always prefer to write your own SQL statements
+  -- this becouse the migration libraries has a bunch of internals assumptions
+  -- that be faster obscure for you and maybe put you on a dark spot where you
+  -- may not have knowledge of whats going on
+
