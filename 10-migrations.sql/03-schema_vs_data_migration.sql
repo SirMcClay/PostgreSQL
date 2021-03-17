437c4c3eb7b030622c7a64f5fb1a1b0631d92768
sirmcclay <mcclay.design@gmail.com>
2021-03-17 12:03:25 -0300

    shcema and data migrations together the right way

--- 10-migrations.sql/03-schema_vs_data_migration.sql
+++ 10-migrations.sql/03-schema_vs_data_migration.sql
@@ -7,3 +7,10 @@
   -- merge, delete, copy, transform, and so on
   -- TLDR: MOVE DATA AROUND
 
+-- SO HOW TO DO SCHEMA AND DATA MIGRATION TOGETHER?
+  -- STEP 1 - Do the schema changes with migration
+  -- STEP 2 - Modify the API and client to work with the old and new schema
+  -- STEP 3 - Do the data changes with or without migration - you seriously
+    -- can do this with just programming approach
+  -- STEP 4 - Update your API and client to work only with new schema
+  -- STEP 5 - Get rid of old schema with migration
