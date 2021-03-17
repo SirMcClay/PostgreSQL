3ce7ceac4bd30cb3518fe5600061115dbfc23576
sirmcclay <mcclay.design@gmail.com>
2021-03-17 10:39:00 -0300

    migration to raname and improve comment

--- 10-schema_migrations.sql/node-project-migration/migrations/1615908506313_table-comments.js
+++ 10-schema_migrations.sql/node-project-migration/migrations/1615908506313_table-comments.js
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 // COMMAND TO EXECUTE MIGRATIONS
+// UP/DOWN --> DATABASE_URL=postgres://USERNAME:PASSWORD@localhost:5432/DATABASE npm run migrate up
 // UP --> DATABASE_URL=postgres://postgres:postgres@localhost:5432/socialnetwork npm run migrate up
 // DOWN --> DATABASE_URL=postgres://postgres:postgres@localhost:5432/socialnetwork npm run migrate down
 
