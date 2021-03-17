3cd6908889b35e0fef09c67e0b5af2ac0d2849c5
sirmcclay <mcclay.design@gmail.com>
2021-03-17 12:32:37 -0300

    route to get posts and a form to create

--- 10-migrations.sql/node-project-migration/index.js
+++ 10-migrations.sql/node-project-migration/index.js
@@ -40,5 +40,21 @@ app.get('/posts', async (req, res) => {
 					.join('')}
       </tbody>
     </table>
+    <form method="POST">
+      <h3>Create Post</h3>
+      <div>
+        <label>Lng</label>
+        <input name="lng" />
+      </div>
+      <div>
+        <label>Lat</label>
+        <input name="lat" />
+      </div>
+      <button type="Submit">Create</button>
+    </form>
   `);
+});
+
+app.listen(3005, () => {
+	console.log('Listening on port 3005');
 });
