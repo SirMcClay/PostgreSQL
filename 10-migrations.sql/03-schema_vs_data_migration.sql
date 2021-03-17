9302854ca0fcc9de6a58e6bfdc02c2a7ebdf7c15
sirmcclay <mcclay.design@gmail.com>
2021-03-17 10:53:38 -0300

    tldr to schema vs data migration

--- 10-migrations.sql/03-schema_vs_data_migration.sql
+++ 10-migrations.sql/03-schema_vs_data_migration.sql
@@ -1,7 +1,9 @@
 -- SCHEMA MIGRATION - Refers all the time to the structure of a database
   -- add table, add column, change type, change column name, merge column
   -- and so on
+  -- TLDR: CHANGE STRUCTURE
 
 -- DATA MIGRATION - Refers all the time to manipulate data in order to move,
   -- merge, delete, copy, transform, and so on
+  -- TLDR: MOVE DATA AROUND
 
