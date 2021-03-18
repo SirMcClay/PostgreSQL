43ae365d50b58695a3396fcc6d8e37528e88181f
sirmcclay <mcclay.design@gmail.com>
2021-03-18 12:35:16 -0300

    just some comments to start work with our routes and prepare to use repository pattern

--- 11-api_access/01-social-repo/src/routes/users.js
+++ 11-api_access/01-social-repo/src/routes/users.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/users', async (req, res) => {});
+router.get('/users', async (req, res) => {
+	// Run a query to get all users
+	// Send the result back to the person
+	// who made this request
+});
 
 router.get('/users/:id', async (req, res) => {});
 
