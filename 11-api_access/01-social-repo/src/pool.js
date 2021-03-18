c727919a9cfc4f1f2eadd7d209b142cc75abdc9d
sirmcclay <mcclay.design@gmail.com>
2021-03-18 12:25:27 -0300

    index.js with connection database and a little validation to run our server just if connection database is ok

--- 11-api_access/01-social-repo/src/pool.js
+++ 11-api_access/01-social-repo/src/pool.js
@@ -1,10 +1,11 @@
 const pg = require('pg');
 
 class Pool {
-	_pool = null;
+	// _pool = null;
 
 	connect(options) {
 		this._pool = new pg.Pool(options);
+		return this._pool.query('SELECT 1 + 1');
 	}
 }
 
