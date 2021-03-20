5cd831d1d460c4e23a6b58a680884f16d0f8fbb9
sirmcclay <mcclay.design@gmail.com>
2021-03-20 17:57:53 -0300

    disconnecting pool after test runs

--- 11-api_access/01-social-repo/src/test/routes/users.test.js
+++ 11-api_access/01-social-repo/src/test/routes/users.test.js
@@ -13,6 +13,10 @@ beforeAll(() => {
 	});
 });
 
+afterAll(() => {
+	return pool.close();
+});
+
 it('create a user', async () => {
 	const startingCount = await UserRepo.count();
 	expect(startingCount).toEqual(0);
